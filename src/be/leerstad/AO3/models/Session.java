05c397a6d35fc8ef4b01ae2fe5bd58df2fab3ec9
eldje <ulrik.vandenbulcke@gmail.com>
2017-02-20 21:22:43 +0100

    first structured tree set

--- src/be/leerstad/AO3/models/Session.java
+++ src/be/leerstad/AO3/models/Session.java
@@ -1,11 +1,14 @@
 package be.leerstad.AO3.models;
 
-
 public class Session {
     private String sessionId;
     private Logfile logFile;
     private IPAddress ipAddress;
     private Datetime sessiontTime;
-    private
+    private int totalTime;
+    private int transferredBytes;
+    private int numberOfRequests;
+    private User user;
+    private Site site;
 
 }
